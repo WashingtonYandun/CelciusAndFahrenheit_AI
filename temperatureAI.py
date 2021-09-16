0d8553e56b0000509032614b79bfa3981ea823ca
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-09-15 23:34:52 -0500

    made model
    model with adam, loss function 'mean_squared_error'

--- temperatureAI.py
+++ temperatureAI.py
@@ -10,3 +10,9 @@ fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 #keras
 layer = tf.keras.layers.Dense(units=1, input_shape=[1]) #dense, all to all this 1-1
 model = tf.keras.Sequential([layer])
+
+#model with Adam
+model.compile(
+    optimizer=tf.keras.optimizers.Adam(0.09),
+    loss='mean_squared_error' #'better few big errors' 
+)
