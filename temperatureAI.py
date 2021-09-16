a3f2c4bec72f8695a09a7df9059d39d2fd7aac9b
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-09-15 23:08:54 -0500

    Layer and model made
    Using keras

--- temperatureAI.py
+++ temperatureAI.py
@@ -6,3 +6,7 @@ import matplotlib.pyplot as plt
 #numpy arrays input and output
 celsius = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
 fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
+
+#keras
+layer = tf.keras.layers.Dense(units=1, input_shape=[1]) #dense, all to all this 1-1
+model = tf.keras.Sequential([layer])
