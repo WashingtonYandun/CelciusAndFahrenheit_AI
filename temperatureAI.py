87f0361590d6c0db72eaca7b612ed25cd8f7e3fc
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-09-15 23:38:42 -0500

    Update temperatureAI.py

--- temperatureAI.py
+++ temperatureAI.py
@@ -8,7 +8,7 @@ celsius = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
 fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 
 #keras
-layer = tf.keras.layers.Dense(units=1, input_shape=[1]) #dense, all to all this 1-1
+layer = tf.keras.layers.Dense(units=1, input_shape=[1]) #dense, all to all this 1 - 1 
 model = tf.keras.Sequential([layer])
 
 #model with Adam
@@ -16,3 +16,8 @@ model.compile(
     optimizer=tf.keras.optimizers.Adam(0.09),
     loss='mean_squared_error' #'better few big errors' 
 )
+
+#review the training
+plt.xlabel("Cicle")
+plt.ylabel("Loss Mag")
+plt.plot(trainingHistorial.history["loss"])
