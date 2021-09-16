e55a6e9dd27421ae546f1730b516341a77087afa
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-09-15 23:03:37 -0500

    Input and output added
    Numpy arrays

--- temperatureAI.py
+++ temperatureAI.py
@@ -1,3 +1,8 @@
+#libraries
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+
+#numpy arrays input and output
+celsius = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
+fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
